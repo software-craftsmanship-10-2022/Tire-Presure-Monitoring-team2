287294714b9a585b7313cd4030ad0b0264c6dbc3
JL <jlpavonmu@gmail.com>
2022-11-30 18:38:47 +0100

    added constants

--- tire-pressure-monitoring-system/alarm.ts
+++ tire-pressure-monitoring-system/alarm.ts
@@ -1,17 +1,16 @@
-import Sensor from './sensor';
-
 export interface IBaseSensor {
 	popNextPressurePsiValue(): number;
 }
-
+const MIN_TRESHOLD = 17;
+const MAX_TRESHOLD = 21;
 export default class Alarm {
+
 	private highPressureThreshold: number;
 	private lowPressureThreshold: number;
-
 	private sensor: IBaseSensor;
 	private alarmOn: boolean;
 
-	constructor(sensor: IBaseSensor, lowPressureThreshold: number = 17, highPressureThreshold: number = 21) {
+	constructor(sensor: IBaseSensor, lowPressureThreshold: number = MIN_TRESHOLD, highPressureThreshold: number = MAX_TRESHOLD) {
 		
 		this.lowPressureThreshold = lowPressureThreshold;
 		this.highPressureThreshold = highPressureThreshold;
